40ce8cc9c5b82f237bd5a5ad73e0e94e41acb425
Eliecer David Pari Alhuay <eparialhuay@gmail.com>
2019-02-11 15:44:39 +0000

    agregando selector de paises

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   #app
     img(src="./assets/logo.png")
     h1 Platzi Music
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
     ul
       artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
 </template>
@@ -14,18 +16,35 @@ export default {
   name: 'app',
   data () {
     return {
-      artists: []
+      artists: [],
+      countries: [
+        { name: 'Argentina', value: 'argentina' },
+        { name: 'Perú', value: 'peru' },
+        { name: 'Colombia', value: 'colombia' },
+        { name: 'España', value: 'spain' },
+      ],
+      selectedCountry: 'argentina'
     }
   },
   components: {
     Artist
   },
+  methods: {
+    refreshArtists() {
+      const self = this
+      getArtists(this.selectedCountry)
+        .then(function (artists) {
+          self.artists = artists
+        })
+    }
+  },
   mounted: function () {
-    const self = this
-    getArtists()
-      .then(function (artists) {
-        self.artists = artists
-      })
+    this.refreshArtists()
+  },
+  watch: {
+    selectedCountry() {
+      this.refreshArtists()
+    }
   }
 }
 </script>
